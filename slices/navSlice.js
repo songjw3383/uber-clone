0bf89180bde210fa7d332bf4819ec5bf2ab0f68c
Song Jaewoo <xoals19958@gmail.com>
2021-08-01 01:54:15 +0900

    0801
    React-native, Redux and Tailwind Set-up

---
+++ slices/navSlice.js
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    origin: null,
+    destination: null,
+    travelTimeInformation: null
+}
+
+export const navSlice = createSlice({
+    name: 'nav',
+    initialState,
+    reducer: {
+        setOrigin: (state, action) => {
+            state.orgin = action.payload;
+        },
+        setDestination: (state, action) => {
+            state.destination = action.payload;
+        },
+        setTravelTimeInformation: (state, action) => {
+            state.travelTimeInformation = action.payload;
+        },
+    },
+});
+
+export const { setOrgin, setDestination, setTravelTimeInformation } = navSlice.actions;
+
+//selectors, means exporting current (changed) state : 현재 아니면 바뀐 state의 값을 export
+export const selectOrgin = (state) => state.nav.orgin;
+export const selectDestination = (state) => state.nav.destination;
+export const selectTravelTimeInformation = (state) => state.nav.travelTimeInformation;
+
+export default navSlice.reducer;
+
